be0cf0ae4fdc8dc61b9b84840f598a318ed2c3f5
Yery Rodriguez <128627025+Yeryalex@users.noreply.github.com>
2026-05-29 11:47:02 +0200

    Append new li articles to ul

---
+++ foundations/shopping-list/index.js
@@ -0,0 +1,25 @@
+const ulRef = document.querySelector("ul");
+const inputRef = document.querySelector("form input");
+const buttonRef = document.querySelector("button");
+
+function addItem(e) {
+    
+    e.preventDefault();
+
+    let itemToAdd = e.target.form.item.value;
+    e.target.form.item.value = "";
+
+
+    let liItem = document.createElement("li");
+    let spanItem = document.createElement("span");
+    let buttonDelete = document.createElement("button");
+
+    liItem.appendChild(spanItem);
+    liItem.appendChild(buttonDelete);
+
+    spanItem.textContent = itemToAdd;
+    buttonDelete.textContent = "Delete"
+    ulRef.appendChild(liItem);
+}
+
+buttonRef.addEventListener("click", addItem);
