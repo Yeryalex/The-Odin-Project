56c9ab0bd7e705bfa3bf9a2dbeeeb6f2b11642c0
Yery Rodriguez <128627025+Yeryalex@users.noreply.github.com>
2026-06-03 09:25:03 +0200

    Add score counter and eventlistener to each button

--- foundations/rock-paper-scissor/index.js
+++ foundations/rock-paper-scissor/index.js
@@ -2,55 +2,73 @@ const containerDiv = document.createElement("div");
 const rockButton = document.createElement("button");
 const paperButton = document.createElement("button");
 const scissorsButton = document.createElement("button");
+const humanPoints = document.createElement("div");
+const computerPoints = document.createElement("div");
+const scoreContainer = document.createElement("div");
+
+let humanScore = 0;
+let computerScore = 0;
 
 rockButton.innerText = "ROCK";
 paperButton.innerText = "PAPER";
 scissorsButton.innerText = 'SCISSORS';
+humanPoints.innerText = humanScore;
+computerPoints.innerText = computerScore;
 
+scoreContainer.appendChild(humanPoints);
+scoreContainer.appendChild(computerPoints);
+
+containerDiv.appendChild(scoreContainer);
+containerDiv.appendChild(rockButton);
+containerDiv.appendChild(rockButton);
 containerDiv.appendChild(rockButton);
 containerDiv.appendChild(paperButton);
 containerDiv.appendChild(scissorsButton);
 
 document.body.appendChild(containerDiv);
 
-playGame();
 
-function playGame() {
-    
-    let humanScore = 0;
-    let computerScore = 0;
 
-    function playRound(humanChoice, computerChoice) {
+containerDiv.addEventListener("click", (e) => {
+
+    if (e.target.tagName == "BUTTON"){
+
+        playRound(e, getComputerChoice());
+        console.log(humanScore);
+        console.log(computerScore);
+    }
+});
+
+
+function playRound(event, computerChoice) {
     
-        if (humanChoice == computerChoice) {
-        
-            console.log("It is a tied! None of you deserve a point.");
-            return ;
-        }
-        if (humanChoice == "ROCK" && computerChoice == "SCISSORS" 
-            || humanChoice == "PAPER" && computerChoice == "ROCK"
-            ||humanChoice == "SCISSORS" && computerChoice == "PAPER")
-        {
-            console.log(`You win! ${humanChoice} beats ${computerChoice}`);
-            humanScore++;
+
+    let humanChoice = event.target.innerText;
+
+    if (humanChoice == computerChoice) {
     
-        } else {
-            console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
-            computerScore++;
-        }
+        console.log("It is a tied! None of you deserve a point.");
+        return ;
+    }
+    if (humanChoice == "ROCK" && computerChoice == "SCISSORS" 
+        || humanChoice == "PAPER" && computerChoice == "ROCK"
+        ||humanChoice == "SCISSORS" && computerChoice == "PAPER")
+    {
+        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+        humanPoints.innerText = humanScore++;
+
+    } else {
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+        computerPoints.innerText = computerScore++;
+    }
+    if (humanScore == 5) {
+        console.log("YOU WIN THE GAME!");
+        return ;
+    }
+    if (computerScore == 5) {
+        console.log("YOU LOSE THE GAME");
     }
 
-    // for (let i = 0; i < 5; i++) {
-
-    //     const humanSelection = getHumanChoice();
-    //     const computerSelection = getComputerChoice();
-    //     playRound(humanSelection, computerSelection);
-    // }
-    // if (humanScore == computerScore) console.log("Oops! It is a tied, nobody wins the game.");
-    // else if (humanScore > computerScore)
-    //     console.log("You win the game!");
-    // else 
-    //     console.log("You lose the game!");
 }
 
 function getComputerChoice() {
@@ -60,9 +78,3 @@ function getComputerChoice() {
 
     return(throwOptions[index]);
 }
-
-function getHumanChoice() {
-    
-    return(prompt("Enter your option!", "Rock, paper or scissors...").toUpperCase());
-}
-
