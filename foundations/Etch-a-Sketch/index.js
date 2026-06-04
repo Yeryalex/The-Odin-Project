bf0de1003b38446a9b45f6a6961bdfd09b537158
Yery Rodriguez <128627025+Yeryalex@users.noreply.github.com>
2026-06-04 09:59:11 +0200

    Create the displayGrid function -Implement displayGrid() function so that creates new grids according to the number of grids passed as argument. -Add a button to ask the user about the number of squares they want to be displayed. -Implement the logic where the grid is displayed, cleaned and changed accordingly when required by the user.

--- foundations/Etch-a-Sketch/index.js
+++ foundations/Etch-a-Sketch/index.js
@@ -1,17 +1,52 @@
+let GRID_SIZE = 600;
+
 const main = document.createElement("main");
 const gridContainer = document.createElement("div");
+const button = document.createElement("button");
 
 gridContainer.classList.add("grid-container");
+gridContainer.style.width = `${GRID_SIZE}px`;
+gridContainer.style.height = `${GRID_SIZE}px`;
+button.innerText = "Change grid size";
+main.appendChild(button);
 main.appendChild(gridContainer);
 document.body.appendChild(main);
 
-for (let i = 0; i < 100 * 100; i++) {
 
-    const gridUnit = document.createElement("div");
-    gridUnit.classList.add("grid-unit");
-    let witdhGrid = 480/100;
-    let heightGrid = 480/100;
-    gridUnit.style.width = `${witdhGrid}px`;
-    gridUnit.style.height = `${heightGrid}px`;
-    gridContainer.appendChild(gridUnit);
+displayGrid(16);
+
+function displayGrid(squaresNumber) {
+
+    for (let i = 0; i < squaresNumber * squaresNumber; i++) {
+
+        const gridUnit = document.createElement("div");
+        gridUnit.classList.add("grid-unit");
+        let witdhGrid = GRID_SIZE / squaresNumber;
+        let heightGrid = GRID_SIZE / squaresNumber;
+        gridUnit.style.width = `${witdhGrid}px`;
+        gridUnit.style.height = `${heightGrid}px`;
+        gridContainer.appendChild(gridUnit);
+    }
+
 }
+
+
+
+gridContainer.addEventListener("mouseover", (e) => {
+
+    if (e.target.tagName === "DIV" && e.target.classList.value == "grid-unit") {
+        e.target.style.background = "red";
+    }
+});
+
+button.addEventListener("click", (e) => {
+
+    let squaresNumber = parseInt(prompt("Change the grid size", "from 1-100"));
+    if (!squaresNumber || squaresNumber < 1 || squaresNumber > 100) {
+        alert("Something went wrong! Please, try a valid value.")
+        return };
+       const gridUnit = document.querySelectorAll(".grid-unit");
+        gridUnit.forEach(div => div.remove())
+
+    displayGrid(parseInt(squaresNumber));
+});
