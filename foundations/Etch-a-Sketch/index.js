a270b1a5979525e37a8140a8b9ce3e4410417224
Yery Rodriguez <128627025+Yeryalex@users.noreply.github.com>
2026-06-04 08:49:07 +0200

    Add grid functionality for 16*16 squared and display

--- foundations/Etch-a-Sketch/index.js
+++ foundations/Etch-a-Sketch/index.js
@@ -1,1 +1,17 @@
-console.log("holamundo")
+const main = document.createElement("main");
+const gridContainer = document.createElement("div");
+
+gridContainer.classList.add("grid-container");
+main.appendChild(gridContainer);
+document.body.appendChild(main);
+
+for (let i = 0; i < 100 * 100; i++) {
+
+    const gridUnit = document.createElement("div");
+    gridUnit.classList.add("grid-unit");
+    let witdhGrid = 480/100;
+    let heightGrid = 480/100;
+    gridUnit.style.width = `${witdhGrid}px`;
+    gridUnit.style.height = `${heightGrid}px`;
+    gridContainer.appendChild(gridUnit);
+}
