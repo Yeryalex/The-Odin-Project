ebd90b28fc86907ed70b42b01477db34fd5a9b6d
Yery Rodriguez <128627025+Yeryalex@users.noreply.github.com>
2026-06-10 18:58:36 +0200

    Refactor input value code to keep it simple

--- foundations/calculator/index.js
+++ foundations/calculator/index.js
@@ -1,6 +1,5 @@
 let operation = [];
 let digits = "";
-let zero = false;
 
 const input = document.querySelector("#calculator");
 const buttonContainer = document.querySelector(".buttonContainer");
@@ -8,10 +7,7 @@ const equalsButton = document.querySelector("#equals");
 
 equalsButton.addEventListener("click", () => {
 
-    if (digits != ""){
-        operation.push(digits)
-        digits = "";
-    };
+    if (digits != "") operation.push(digits);
     console.log(operation);
 
 })
@@ -19,49 +15,22 @@ equalsButton.addEventListener("click", () => {
 buttonContainer.addEventListener("click", (e) => {
 
     if (e.target.className == "digits") {
-        
-        digits += e.target.textContent;
-        console.log(digits)
 
-        if (input.value == "0") {
-            input.value = "";
-            input.value += e.target.textContent;
-            return ;
-        };
+        if (digits == "0") digits = "";
+        digits += e.target.textContent;
+
+        if (input.value == "0") input.value = "";
         input.value += e.target.textContent;
-            
-        };
-    
+    }
     if (e.target.className == "operator") {
 
-        console.log(digits);
-        if (digits != "0"){
-            operation.push(digits)
-        };
-        input.value += e.target.textContent;
-        operation.push(e.target.textContent);
+        if (digits != "") operation.push(digits);
         digits = "";
-       
+        operation.push(e.target.textContent);
+        input.value += e.target.textContent;
     }
+    console.log(digits);
 })
-
-// function tokenizer(stringOperation) {
-
-//     let numberOperand = "";
-//     let splitValues = stringOperation.split("");
-//     for (let i = 0; i < splitValues.length; i++) {
-//         if (!isNaN(splitValues[i]))
-//             numberOperand += splitValues[i];
-
-//         if (isNaN(splitValues[i])) {
-//             if (numberOperand != "") operators.push(numberOperand);
-//             operators.push(splitValues[i]);
-//             numberOperand = "";
-//         }
-//     }
-//     if (numberOperand != "") operators.push(numberOperand);
-//     console.log(operators);
-// }
 
 function add(a, b) {
     return (a + b);
