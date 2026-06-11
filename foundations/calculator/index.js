3df7ac6f788cfb3ef1848d1d312c31c195d7cb9c
Yery Rodriguez <128627025+Yeryalex@users.noreply.github.com>
2026-06-11 08:07:58 +0200

    Add conditional checks for zero values in input.value

--- foundations/calculator/index.js
+++ foundations/calculator/index.js
@@ -17,42 +17,29 @@ equalsButton.addEventListener("click", () => {
 
 buttonContainer.addEventListener("click", (e) => {
 
+    let content = e.target.textContent;
     if (e.target.className == "digits") {
 
-        if (digits == "0") digits = "";
-        digits += e.target.textContent;
-
-        if (input.value == "0") input.value = "";
-
-        if (digits != "0") {
-            if (cleanZero) {
-
-                cleanZero = false;
-                input.value = input.value.slice(0, input.value.length - 1);
-                input.value += e.target.textContent;
-
-            }
-            else
-            {
-                input.value += e.target.textContent;
-
-            }
-        }
-        else {
-            
-            if (input.value.lastIndexOf("0") == -1){
-                input.value += e.target.textContent;
-                cleanZero = true;
-            }
-
-        }
+       if (digits === "0" && content === "0") return ;
+       if (input.value === "0") input.value = "";
+      
+       if (digits === "0" && content !== "0") {
+        
+        digits = "";
+        if (input.value.endsWith("0")) {
+            input.value = input.value.slice(0, -1);
+        } 
+       }
+       input.value += content;
+        digits += content;
     }
     if (e.target.className == "operator") {
 
-        if (digits != "") operation.push(digits);
+        if (digits === "" && operation.length === 0) return;
+        if (digits !== "") operation.push(digits);
         digits = "";
-        operation.push(e.target.textContent);
-        input.value += e.target.textContent;
+        operation.push(content);
+        input.value += content;
     }
 })
 
