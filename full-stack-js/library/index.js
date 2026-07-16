8ec690b0a276a7319f7b85384020639659199d33
Yery Rodriguez <128627025+Yeryalex@users.noreply.github.com>
2026-07-16 09:05:25 +0200

    Create displayCards function loop and  to generate during runtime cards to be appended to the card-container

--- full-stack-js/library/index.js
+++ full-stack-js/library/index.js
@@ -3,6 +3,7 @@ const author = document.querySelector('#author');
 const pages = document.querySelector('#pages');
 const isRead = document.querySelector('#read');
 const addBookForm = document.querySelector('#book-form');
+const cardContainer = document.querySelector('.card-container');
 
 const myLibrary = [];
 
@@ -24,17 +25,54 @@ Book.prototype.info = function () {
 };
 
 
-function addBookToLibrary(objectDetailsBook) {
-  myLibrary.push(objectDetailsBook);
-}
-
-
 addBookForm.addEventListener("submit", (e) => {
 
     // e.preventDefault();
-
+    while (cardContainer.firstChild) {
+        cardContainer.removeChild(cardContainer.firstChild);
+    }
+    
     addBookToLibrary(new Book(title.value, author.value, pages.value, isRead.checked));
-    console.log(myLibrary);
+    displayCards(myLibrary);
+    
     addBookForm.reset();
 });
 
+function addBookToLibrary(objectDetailsBook) {
+    myLibrary.push(objectDetailsBook);
+}
+
+function displayCards(myLibrary) {
+    myLibrary.forEach(element => {
+        
+
+        const card = document.createElement("div");
+        const tittleCard = document.createElement("div");
+        const infoCard = document.createElement("div");
+        const pAuthor = document.createElement("p");
+        const pPages = document.createElement("p");
+        const pRead = document.createElement("p");
+        
+        tittleCard.innerText = element.title;
+        pAuthor.innerText = element.author;
+        pPages.innerText = element.pages;
+        pRead.innerText = element.read;
+
+        card.classList.add("card");
+        tittleCard.classList.add("book-title");
+        infoCard.classList.add("info-card");
+        pAuthor.classList.add("author");
+        pPages.classList.add("pages");
+        pRead.classList.add("read");
+        
+        infoCard.appendChild(pAuthor);
+        infoCard.appendChild(pPages);
+        infoCard.appendChild(pRead);
+        
+        card.appendChild(tittleCard);
+        card.appendChild(infoCard);
+        
+        cardContainer.appendChild(card);
+
+    });
+}
